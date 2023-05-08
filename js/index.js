2422b819998135eab1ff208c649805396d62ea52
Katya Kuripka <katyakuripkav@gmail.com>
2023-05-08 13:53:03 +0300

    oll

--- js/index.js
+++ js/index.js
@@ -1,46 +1,31 @@
-// const swiper = new Swiper('.slider-lot', {
-//     // Optional parameters
-//     loop: true,
-//     slidesPerView: 2,
-//     spaceBetween: 30,
-//
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-//
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//
-//     // And if we need scrollbar
-//     scrollbar: {
-//         el: '.swiper-scrollbar',
-//     },
-//     // slideToClickedSlide: true,
-//     // autoHeight: true,
-// });
-//
 $('.slider-lot').slick({
     infinite: true,
     slidesToShow:2,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     arrows: true,
     dots: true,
     responsive: [
         {
-            breakpoint: 768,
+            breakpoint: 1025,
             settings: {
                 dots: false,
                 arrows: false,
-                slidesToShow: 3,
+                slidesToShow: 2,
                 autoplay: true,
-                autoplaySpeed: 2000,
+                autoplaySpeed: 3000,
+                adaptiveHeight:true,
+            }
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                dots: false,
+                arrows: false,
+                slidesToShow: 2,
+                autoplay: true,
+                autoplaySpeed: 3000,
                 adaptiveHeight:true,
             }
         },
@@ -50,7 +35,7 @@ $('.slider-lot').slick({
                 arrows: false,
                 slidesToShow: 1,
                 autoplay: true,
-                autoplaySpeed: 2000,
+                autoplaySpeed: 3000,
                 adaptiveHeight:true,
             }
         }
@@ -60,31 +45,49 @@ $('.slider-lot-2').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 3000,
     arrows: true,
     dots: true,
     responsive: [
         {
-            breakpoint: 768,
+            breakpoint: 1025,
             settings: {
                 dots: false,
                 arrows: false,
-                slidesToShow: 3,
+                slidesToShow: 2,
                 autoplay: true,
-                autoplaySpeed: 2000,
+                autoplaySpeed: 3000,
                 adaptiveHeight:true,
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 769,
+            settings: {
+                dots: false,
+                arrows: false,
+                slidesToShow: 2,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                adaptiveHeight:true,
+            }
+        },
+        {
+            breakpoint: 481,
             settings: {
                 arrows: false,
                 slidesToShow: 1,
                 autoplay: true,
-                autoplaySpeed: 2000,
+                autoplaySpeed: 3000,
                 adaptiveHeight:true,
             }
         }
     ]
+});
+
+$( document ).ready(function() {
+    $(".burger-button").click(function(){
+        $(".burger-button").toggleClass("active");
+        $(".burger-menu").toggleClass("active");
+    });
 });
